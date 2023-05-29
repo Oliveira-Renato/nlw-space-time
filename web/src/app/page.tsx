81ee09736de6d3e1fdba88336596581902e1f068
Renato <renato.printf@gmail.com>
2023-05-28 21:04:10 -0300

    starting web using next.js

---
+++ web/src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/Button"
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <Button title="Click me"/>
+      <Button title="Don't click me"/>
+      <Button title="hit me"/>
+    </div>
+  )
+}
