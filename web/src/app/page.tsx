612c34c43fe80eb8434d597820c5c095e47ad761
Renato <renato.printf@gmail.com>
2023-06-04 21:23:10 -0300

    config tailwind

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,11 +1,9 @@
-import { Button } from "@/components/Button"
 export default function Home() {
   return (
     <div>
-      <h1>Hello World!</h1>
-      <Button title="Click me"/>
-      <Button title="Don't click me"/>
-      <Button title="hit me"/>
+      <p className="text-2xl bold bg-red-500 w-40 h-40 rounded flex items-center justify-center m-4">
+        Hello World!
+      </p>
     </div>
   )
 }
